66ffefea4e1cb1d26d606fa38c8f5e9f41780af0
liunan13 <liunan13@xdf.cn>
2021-06-03 14:04:08 +0800

    add algoriathm

--- src/main/java/com/istella/algoriathm/CountAndSay.java
+++ src/main/java/com/istella/algoriathm/CountAndSay.java
@@ -12,7 +12,39 @@ public class CountAndSay {
         System.out.println(s);
     }
 
+    /**
+     * 给定一个正整数 n ，输出外观数列的第 n 项。
+     *
+     * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+     * @param n
+     * @return
+     */
     public static String countAndSay(int n) {
-        return "";
+        String r="1";
+        int p=0;
+        char c='1';
+        int total=0;
+        for(int i=1;i<n;i++){
+            char[] chars=r.toCharArray();
+            r="";
+            while (p<chars.length){
+                if (chars[p] == c) {
+                    total++;
+                } else {
+                    r+=total;
+                    r+=c;
+                    total=1;
+                    c=chars[p];
+                }
+                p++;
+            }
+            r+=total;
+            r+=c;
+            total=0;
+            p=0;
+            c=r.charAt(0);
+        }
+
+        return r;
     }
 }
